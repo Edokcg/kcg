bb096084bfe126b33250b2b1c10d55f6aa9e8981
knight00 <perfectdicky@yahoo.com.hk>
2025-12-02 22:19:13 +0800

    fix zarc

--- script/kcg/c703.lua
+++ script/kcg/c703.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.AND(s.splimit,aux.fuslimit))
+	e1:SetValue(aux.OR(s.splimit,aux.fuslimit))
 	c:RegisterEffect(e1)
 	
 	--act limit
