98e0c5263a6635d6a3e50a21e868d9454a926315
Yang4834 <125434496+Yang4834@users.noreply.github.com>
2026-02-21 10:47:12 +0800

    updata

---
+++ script/Pre-Errata/c852.lua
@@ -0,0 +1,78 @@
+--遗言状(Pre-Errata)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	if not s.global_check then
+		s.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_TO_GRAVE)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=eg:FilterCount(Card.IsMonster,nil)
+	if ct==0 then return end
+	if Duel.GetFlagEffect(0,id)==0 then
+		Duel.RegisterFlagEffect(0,id,RESET_PHASE|PHASE_END,0,1,ct)
+		Duel.RegisterFlagEffect(1,id,RESET_PHASE|PHASE_END,0,1,ct)
+	else
+		local ct1=Duel.GetFlagEffectLabel(0,id)
+		local ct2=Duel.GetFlagEffectLabel(0,id)
+		Duel.SetFlagEffectLabel(0,id,ct1+ct)
+		Duel.SetFlagEffectLabel(1,id,ct2+ct)
+	end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if Duel.GetFlagEffect(tp,id)~=0 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		e:SetLabel(1)
+	else
+		e:SetCategory(0)
+		e:SetLabel(0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	else
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCondition(s.spcon)
+		e1:SetOperation(s.spop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsAttackBelow(1500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetFlagEffectLabel(tp,id)
+	return ct and ct>=1
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetFlagEffectLabel(tp,id)
+	Duel.SetFlagEffectLabel(tp,id,ct-1)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+end
